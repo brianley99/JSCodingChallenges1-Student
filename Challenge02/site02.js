55d6e5febc0c0b08489c04b64c8cc4456d0696c7
brianley99 <Brianley99@gmail.com>
2024-05-07 21:51:26 -0700

    Challange 02 Complete

--- Challenge02/site02.js
+++ Challenge02/site02.js
@@ -35,6 +35,57 @@ function checkBrackets() {
 //takes an array of strings and returns the longest one. 
 function isBalanced(brackets) {
 
-   return false;
+    // Declare array to store open brackets into
+    let stackOfBrackets = [];
+
+    // Define open/closed brackets
+    const openTypeBrackets = ['(', '{', '['];
+    const closedTypeBrackets = [')', '}', ']'];
+
+    // Loop over all the brackets
+    for (let i = 0; i < brackets.length; i++) {
+        
+        const currentBracket = brackets[i];
+        
+        if (openTypeBrackets.includes(currentBracket)) {
+            
+            // Add open brackets to array
+            stackOfBrackets.push(currentBracket);
+        }
+        else{
+
+            // Compare closed bracket to the last one in the array
+            
+            // Get last open bracket (this also removes it from the stack)
+            const lastOpenBracket = stackOfBrackets.pop();;
+
+            // Find what type is the last open bracket
+            const isTypeOfOpenBracket = (element) => element == lastOpenBracket;
+            let indexOfLastOpenBracket = openTypeBrackets.findIndex(isTypeOfOpenBracket);
+
+            // Find what type is the current bracket
+            const isTypeOfClosedBracket = (element) => element == currentBracket;
+            let indexOfCurrentBracket = closedTypeBrackets.findIndex(isTypeOfClosedBracket);
+
+            // Check if the match
+            if (indexOfLastOpenBracket != indexOfCurrentBracket) {
+                
+                // It's a not match, therefore brackets are unbalanced
+                return false;
+            }
+        }
+        
+    }
+
+    // Check if there were any brackets left over
+    if (stackOfBrackets.length == 0) {
+        
+        return true;
+
+    } else {
+        
+        return false;
+    }
+
 
 }
