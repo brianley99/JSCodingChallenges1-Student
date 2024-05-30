1c24f57bebf750fe3deae8bf791565d3c9a85a73
brianley99 <Brianley99@gmail.com>
2024-05-30 11:51:07 -0700

    Challange complete

--- Challenge05/site05.js
+++ Challenge05/site05.js
@@ -1,9 +1,9 @@
 //driver function used for display and passing values.
 function checkAnagram() {
 
-     //grab the two strings from the ui. no need to change
-     //clear the results
-     document.getElementById("results").innerHTML = "";
+    //grab the two strings from the ui. no need to change
+    //clear the results
+    document.getElementById("results").innerHTML = "";
 
     stringA = document.getElementById("wordOne").value;
     stringB = document.getElementById("wordTwo").value;
@@ -13,11 +13,11 @@ function checkAnagram() {
         return;
     }
 
-   
+
     //implement the isAnagram function
     let anagram = isAnagram(stringA, stringB);
 
-    
+
     //used for display no need to change.
     let msg = "";
 
@@ -33,5 +33,31 @@ function checkAnagram() {
 //takes an two strings and determines if the are anagarms
 //returns true or false
 function isAnagram(stringA, stringB) {
-   return false;
+
+    // Normalze text to lowecase with no spaces
+    let normalizedStrA = stringA.replace(/[^a-zA-Z]/g, '');
+    normalizedStrA = normalizedStrA.toLowerCase();
+
+    let normalizedStrB = stringB.replace(/[^a-zA-Z]/g, '');
+    normalizedStrB = normalizedStrB.toLowerCase();
+
+    // Check if they are the same length
+    if (normalizedStrA.length != normalizedStrB.length) {
+
+        return false;
+    }
+
+    // Order them aphabetically
+    let sortedStrA = normalizedStrA.split('').sort().join('');
+    let sortedStrB = normalizedStrB.split('').sort().join('');
+
+    // Compare them
+    if (sortedStrA == sortedStrB) {
+        
+        return true
+
+    } else {
+        
+        return false;
+    }
 }
